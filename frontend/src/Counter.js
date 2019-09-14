95fa1f7f578d9c719eb84c7d9b931b5c9da6697f
Angela Luo <ael49021@gmail.com>
2019-09-14 15:54:33 -0500

    buttons

--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
-
 class Counter extends Component {
   // YOUR CODE GOES BELOW
-  
+  state = {count: 0}
+    handleAddClick() {
+      this.setState({count : this.state.count + 1})
+    }
+    handleSubtractClick() {
+      this.setState({count : this.state.count -1 })
+    }
+   
   render() {
+    
+    const {count} = this.state
+    
+    
     return (
-      <div/>
+      
+      <div>
+      <label
+        style = {{fontSize: '5em', display: 'block'}}
+      >
+        Count: {count}
+      </label>
+      <button onClick={() => this.handleAddClick()}>Add</button>
+      <button onClick={() => this.handleSubtractClick()}>Subtract</button>
+     
+    </div>
+    
     )
   }
 }
-
 export default Counter
+
+
--- frontend/src/Instructions.js
+++ frontend/src/Instructions.js
@@ -8,7 +8,7 @@ class Instructions extends Component {
           Pass a 'complete' prop to the instructions component in <code>src/App.js</code>. See the README for more details. If you are successful, you'll see another line below saying you've completed this part.
         </p>
         <br />
-        <p hidden={!this.props.complete}>
+        <p hidden={this.props.complete}>
           Completed Part 1!
         </p>
       </div>
@@ -17,3 +17,41 @@ class Instructions extends Component {
 }
 
 export default Instructions
+//COUNTER
+//ISSUE WITH COUNTER NOT RUNNING IF ITS NOT IN THIS FILE
+/*
+
+import React, { Component } from 'react'
+class Counter extends Component {
+  // YOUR CODE GOES BELOW
+  state = {count: 0}
+    handleAddClick() {
+      this.setState({count : this.state.count + 1})
+    }
+    handleSubtractClick() {
+      this.setState({count : this.state.count -1 })
+    }
+   
+  render() {
+    
+    const {count} = this.state
+    
+    
+    return (
+      
+      <div>
+      <label
+        style = {{fontSize: '5em', display: 'block'}}
+      >
+        Count: {count}
+      </label>
+      <button onClick={() => this.handleAddClick()}>Add</button>
+      <button onClick={() => this.handleSubtractClick()}>Subtract</button>
+     
+    </div>
+    
+    )
+  }
+}
+export default Counter
+*/
